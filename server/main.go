7e58d122dccf8b67c1b10069cc13f12d6639aa19
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2021-12-02 19:11:01 +0900

    all reset file

--- server/main.go
+++ server/main.go
@@ -1,40 +1,11 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
-	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 )
 
-type User struct {
-	Id       uint   `gorm:primary_key;not null;unique`
-	Name     string `gorm:"size:255;not null;unique"`
-	Password string
-}
-
-var db *gorm.DB
-
-func init() {
-	db, err := gorm.Open("postgres", "host=postgres port=5432 user=postgres dbname=chat password=chat sslmode=disable")
-	if err != nil {
-		log.Fatalln("not connect", err)
-	}
-	defer db.Close()
-
-	db.AutoMigrate(&User{})
-}
-
 func main() {
-	server := http.Server{
-		Addr: ":8080",
-	}
-	http.HandleFunc("/create", createUserHandler)
-	http.HandleFunc("/login", loginHandler)
-	http.HandleFunc("/main", mainHandler)
-	http.HandleFunc("/delete", deleteHandler)
-	http.HandleFunc("/logout", logoutHandler)
-	http.HandleFunc("/afterlogout", afterlogoutHandler)
-	server.ListenAndServe()
+	http.ListenAndServe(":8080", nil)
 }
