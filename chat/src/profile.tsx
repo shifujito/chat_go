3b99c58387e6f78f8fb05d68085ef1715c9cd246
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-01-04 17:05:20 +0900

    first commit 2022 (logout metho)

--- chat/src/profile.tsx
+++ chat/src/profile.tsx
@@ -1,3 +1,11 @@
+import { Link } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
+
 export const Profile: React.VFC = () => {
-  return <a>s</a>;
+  // ログアウト機能を実装
+  const history = useHistory();
+  const handelLogout = () => {
+    history.push("/login");
+  };
+  return <Link onClick={handelLogout}>ログアウト</Link>;
 };
