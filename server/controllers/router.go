63abfc71cca3998cd8cf9bc4627d9daba6d51504
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-06-19 11:12:38 +0900

    [wip] refactor

--- server/controllers/router.go
+++ server/controllers/router.go
@@ -4,9 +4,5 @@ import "net/http"
 
 func Run() {
 	http.HandleFunc("/users", UserHandler)
-	// http.HandleFunc("/api/login", apiLoginHandler)
-	// http.HandleFunc("/api/posts", apiPostsHandler)
-	// http.HandleFunc("/api/post/delete/", apiPostDeleteHandler)
-	// http.HandleFunc("/api/post/create", apiPostCreateHandler)
 	http.ListenAndServe(":8080", nil)
 }
