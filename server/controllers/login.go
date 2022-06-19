9067ccd74428059aab795d670e06060ed291cb2f
fujitoshion <43021337+FUJITOSHION@users.noreply.github.com>
2022-06-19 12:00:31 +0900

    auto commit

---
+++ server/controllers/login.go
@@ -0,0 +1,65 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/shifujito/chat_go/server/model"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type TryLoginUser struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	corsSetup(w)
+	if r.Method == "POST" {
+		checkNameAndPassword(w, r)
+	}
+
+}
+
+func corsSetup(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Content-Type", "application/json")
+}
+
+func checkNameAndPassword(w http.ResponseWriter, r *http.Request) {
+	var tryLoginUser TryLoginUser
+	var findUser model.User
+
+	// bodyの中身を取得
+	body := make([]byte, r.ContentLength)
+	r.Body.Read(body)
+
+	// paramをjsonにparse
+	json.Unmarshal(body, &tryLoginUser)
+
+	// dbに接続
+	db := model.DbConnect()
+	// 名前から名前とパスワードを取得
+	err := db.Where("name = ?", tryLoginUser.Name).First(&findUser).Error
+	if err != nil {
+		// return 401
+		w.WriteHeader(401)
+		return
+	}
+	defer db.Close()
+	// パスワードが一致するか確認
+	err = bcrypt.CompareHashAndPassword(findUser.Password, []byte(tryLoginUser.Password))
+	if err != nil {
+		// return 401
+		w.WriteHeader(401)
+		return
+	}
+	// 一致した場合ログイン情報をjsonで返す
+	loginUser := User{Id: findUser.ID, Name: findUser.Name}
+	loginUserJson, _ := json.Marshal(loginUser)
+	w.Write(loginUserJson)
+
+}
--- server/controllers/router.go
+++ server/controllers/router.go
@@ -4,5 +4,6 @@ import "net/http"
 
 func Run() {
 	http.HandleFunc("/users", UserHandler)
+	http.HandleFunc("/login", LoginHandler)
 	http.ListenAndServe(":8080", nil)
 }
